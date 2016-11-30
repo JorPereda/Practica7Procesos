37fe87ff29910579e15e19911cea5d1c8547f19d
Jorge <jorgepereda1989@gmail.com>
2016-11-30 12:24:43 +0100

    Articulos

--- SupermercadoCommon/src/main/java/Interfaces/IArticulosDAO.java
+++ SupermercadoCommon/src/main/java/Interfaces/IArticulosDAO.java
@@ -1,6 +1,16 @@
 package Interfaces;
 
+import java.util.List;
+
+import Dominio.Articulo;
 
 public interface IArticulosDAO {
+	
+	Articulo getArticulo(int id);
+	Articulo getArticuloNombre(String nombre);
+	Articulo creaArticulo(Articulo art);
+	Articulo actualizaArticulo(Articulo nuevoArticulo);
+	Articulo eliminaArticulo(Articulo art);
+	List<Articulo> listaArticulo();
 
 }
