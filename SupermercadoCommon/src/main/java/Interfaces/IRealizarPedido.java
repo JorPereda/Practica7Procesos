9b9acb0b11374b7e640f5787fd160a50b8155fa6
Alexandru Guzun <agu20@alumnos.unican.es>
2016-11-30 12:25:35 +0100

    Actualizar metodos interfaces

--- SupermercadoCommon/src/main/java/Interfaces/IRealizarPedido.java
+++ SupermercadoCommon/src/main/java/Interfaces/IRealizarPedido.java
@@ -1,6 +1,9 @@
 package Interfaces;
-
+import Dominio.Pedido;
+import Dominio.Articulo;
 
 public interface IRealizarPedido {
-
+	boolean realizarPedido(String dni);
+	boolean asignaArticulo(Pedido p,Articulo a,int cantidad);
+	int confirmaPedido(Pedido p);
 }
--- SupermercadoCommon/src/main/java/Interfaces/IRegistro.java
+++ SupermercadoCommon/src/main/java/Interfaces/IRegistro.java
@@ -1,6 +1,7 @@
 package Interfaces;
 
+import Dominio.Usuario;
 
 public interface IRegistro {
-
+	boolean registraUsuario(Usuario u);
 }
