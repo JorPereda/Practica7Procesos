88fa60000958631a3c46fb1deee468dc756e3983
Alexandru Guzun <agu20@alumnos.unican.es>
2016-12-14 11:51:56 +0100

    Pruebas unitarias para los metodos de la clase Gestion Usuarios

--- SupermercadoBusiness/src/test/java/pruebasUnitarias/TestUnitarios.java
+++ SupermercadoBusiness/src/test/java/pruebasUnitarias/TestUnitarios.java
@@ -3,9 +3,10 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import Interfaces.IArticulosDAO;
-import Interfaces.IPedidosDAO;
-import Interfaces.IUsuariosDAO;
+import Dominio.Usuario;
+import Interfaces.IRegistro;
+import Interfaces.IUsuarios;
+import negocio.GestionUsuarios;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -13,19 +14,34 @@ import org.junit.Test;
 
 public class TestUnitarios {
 	
-	private static IPedidosDAO		pedidosDAO		= 	mock(IPedidosDAO.class);
+	private static IUsuarios		usuariosI;
+	private static IRegistro		registroI;
+	private static Usuario u;
+	private static GestionUsuarios gU;
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
+		usuariosI		= 	mock(IUsuarios.class);
+		registroI		=	mock(IRegistro.class);
+		u = new Usuario("Pepe","1234567M","Calle del sol");
+		gU = new GestionUsuarios();
+		// Programar para que se utilice el mock correspondiente para "usu" y "reg"
+		gU.usu = usuariosI;
+		gU.reg = registroI;
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// Programar el valor a devolver en caso de invocacion del metodo getUsuario("Pepe")
+		when(gU.getUsuario("Pepe")).thenReturn(u);
+		// Programar el valor a devolver en caso de invocacion del metodo registraUsuario(u)
+		when(gU.registraUsuario(u)).thenReturn(true);
+		// Invocar los metodos de la clase Gestion Usuarios con los valores adecuados
+		gU.getUsuario("Pepe");
+		gU.registraUsuario(u);
+		// Comprobar que los valores devueltos después de la invocación de cada metodo es el esperado
+		assertTrue(gU.getUsuario("Pepe").equals(u));
+		assertTrue(gU.registraUsuario(u)==true);
 	}
 
 }
